4df03f501499b429e656873dccd1512210d992e8
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-03 17:30:25 +0200

    Bonus: added jumbotron

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero">
+    <img src="@/assets/jumbo.png" alt="jumbo-Vue" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+.hero {
+  background-color: #35495e;
+  height: 451px;
+  margin-top: 156px;
+}
+img {
+  margin-top: 150px;
+  width: 800px;
+}
+</style>
